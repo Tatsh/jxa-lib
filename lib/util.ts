0a7f50298cd767b5a73d899c103b739bb80e31ad
Andrew Udvare <audvare@gmail.com>
2019-03-07 05:23:15 -0500

    util: add chr() and ord() functions

--- lib/util.ts
+++ lib/util.ts
@@ -3,3 +3,7 @@ export const throwErrorIfNotNil = (error?: NSError) => {
         throw new Error((error as NSError).localizedDescription.js);
     }
 };
+
+export const chr = (x: number) => String.fromCharCode(x);
+export const ord = (xs: string | string[]): number =>
+    String.prototype.charCodeAt.call(xs, 0);
