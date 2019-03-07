b6577c2bec9542667e18672216a324c5e66431fe
Andrew Udvare <audvare@gmail.com>
2019-03-07 10:53:01 -0500

    Nitpick

--- examples/refresh-selected-tags.ts
+++ examples/refresh-selected-tags.ts
@@ -5,16 +5,13 @@ Refresh Selected Tags v1.0
 June 6, 2006 (yes, that's 6/6/06)
 http://marv.kordix.com
 *)
-
 tell application "iTunes"
-
 	if selection is {} then
         display dialog "No tracks selected. Select some tracks with some
                         stale tags."
             buttons {"I will!"}
 			default button 1 with icon 0
 	else
-
 		set sel to selection
 		repeat with myTrack in sel
 			if myTrack's class is file track then
@@ -23,13 +20,10 @@ tell application "iTunes"
 				end tell
 			end if
 		end repeat
-
         display dialog "Selected tags have been freshened up."
             buttons {"Okay"}
 			default button 1
-
 	end if
-
 end tell
 */
 
