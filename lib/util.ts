b559e6c454c12d9adf0eb88a00a53daa5d0c5a89
Andrew Udvare <audvare@gmail.com>
2019-03-07 23:48:53 -0500

    Remove isAppRunning; no longer necessary

--- lib/util.ts
+++ lib/util.ts
@@ -7,18 +7,3 @@ export const throwErrorIfNotNil = (error?: NSError) => {
 export const chr = (x: number) => String.fromCharCode(x);
 export const ord = (xs: string | string[]): number =>
     String.prototype.charCodeAt.call(xs, 0);
-
-export const isAppRunning = (bundleId: string) => {
-    const runningApps = ObjC.unwrap(
-        $.NSWorkspace.sharedWorkspace.runningApplications);
-    for (const app of runningApps) {
-        if (typeof app.bundleIdentifier.isEqualToString === 'undefined') {
-            continue;
-        }
-        if (app.bundleIdentifier.isEqualToString(bundleId)) {
-            return true;
-        }
-    }
-
-    return false;
-};
