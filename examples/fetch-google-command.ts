17ce3f425fa5c1df2f8a15c5f36cb05a9cf62dac
Andrew Udvare <audvare@gmail.com>
2019-10-12 09:51:17 -0400

    Update fetch example

--- examples/fetch-google-command.ts
+++ examples/fetch-google-command.ts
@@ -1,21 +1,18 @@
 import { DispatchSemaphore } from '../lib/dispatch';
 import fetch from '../lib/fetch';
 import { exit } from '../lib/stdlib';
+import { stringWithData } from '../lib/string';
 import { sleep } from '../lib/unistd';
 
 const main = () => {
   const sema = new DispatchSemaphore(0);
 
   fetch('https://www.google.com/').then(x => {
-    console.log(
-      ObjC.unwrap(
-        $.NSString.alloc.initWithDataEncoding(x.data!, $.NSASCIIStringEncoding)
-      )
-    );
+    console.log(stringWithData(x.data!));
     sema.signal();
   });
 
-  while (sema.wait($.DISPATCH_TIME_FOREVER) > 0) {
+  while (sema.waitForever() != 0) {
     sleep(1);
   }
   return 0;
