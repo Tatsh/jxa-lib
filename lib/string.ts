d7ed3bd04387af6029ea170763fb6e5b0a740446
Andrew Udvare <audvare@gmail.com>
2019-10-12 09:50:54 -0400

    lib/string: add stringWithData()

--- lib/string.ts
+++ lib/string.ts
@@ -4,3 +4,8 @@ export const memchr = $.memchr;
 export const memcmp = $.memcmp;
 export const memcpy = $.memcpy;
 export const memset = $.memset;
+
+export const stringWithData = (
+  data: NSData,
+  encoding = $.NSASCIIStringEncoding
+) => ObjC.unwrap($.NSString.alloc.initWithDataEncoding(data, encoding));
