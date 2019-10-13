dbeefa58a2fa19de61fe69943f9f03de8f329c81
Andrew Udvare <audvare@gmail.com>
2019-10-12 23:58:41 -0400

    util: add applicationWithStandardAdditions function

--- lib/util.ts
+++ lib/util.ts
@@ -1,9 +1,26 @@
 export const throwErrorIfNotNil = (error?: NSError) => {
-    if (!(error as NSError).isNil()) {
-        throw new Error((error as NSError).localizedDescription.js);
-    }
+  if (error && !(error as NSError).isNil()) {
+    throw new Error((error as NSError).localizedDescription.js);
+  }
 };
+
+export function applicationWithStandardAdditions(
+  spec: 'Finder'
+): FinderApplication;
+export function applicationWithStandardAdditions(
+  spec: 'iTunes'
+): ItunesApplication;
+export function applicationWithStandardAdditions(
+  spec: 'Music'
+): ItunesApplication;
+export function applicationWithStandardAdditions(
+  spec: number | string
+): Application {
+  const app = Application(spec);
+  app.includeStandardAdditions = true;
+  return app;
+}
 
 export const chr = (x: number) => String.fromCharCode(x);
 export const ord = (xs: string | string[]): number =>
-    String.prototype.charCodeAt.call(xs, 0);
+  String.prototype.charCodeAt.call(xs, 0);
