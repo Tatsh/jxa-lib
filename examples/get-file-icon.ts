030e92bb3686b47887c92aad2937f61c8d02238f
Andrew Udvare <audvare@gmail.com>
2019-12-06 23:59:10 -0500

    Update example

--- examples/get-file-icon.ts
+++ examples/get-file-icon.ts
@@ -2,11 +2,10 @@ import { Workspace } from '../lib/nsworkspace';
 
 /** Demonstrates calling `[NSWorkspace iconForFile:]`. */
 export default function getIconOfChromeAppsDirectory() {
-  const ws = new Workspace();
   const p = $.NSHomeDirectory()
     .stringByAppendingPathComponent("Applications")
     .stringByAppendingPathComponent("Chrome Apps.localized");
-  const image = ws.iconForFile(p);
+  const image = Workspace.shared.iconForFile(p);
   const tiff = image.TIFFRepresentation;
   const r = $.NSBitmapImageRep.imageRepWithData(tiff);
   const data = r.representationUsingTypeProperties(
