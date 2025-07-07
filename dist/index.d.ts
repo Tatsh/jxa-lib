0abeb7ff00d93acc3f53348a9763b08f3b63f07c
Andrew Udvare <audvare@gmail.com>
2025-07-07 14:45:04 -0400

    Update description

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * as dispatch from './dispatch';
-export * from './fetch';
-export * from './nsfilemanager';
-export * from './nspasteboard';
-export * from './nsworkspace';
-export * as stdlib from './stdlib';
-export * as string from './string';
-export * as unistd from './unistd';
-export * from './util';
