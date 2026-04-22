0c76574db3ceb13906683ce4cf2fe0aada18a7ed
Andrew Udvare <audvare@gmail.com>
2026-04-22 09:04:48 -0400

    cruft: update
    Signed-off-by: Andrew Udvare <audvare@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,14 @@
 import js from '@eslint/js';
-import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default defineConfig(
-  [
-    globalIgnores(['coverage', 'dist']),
-    { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'] },
-    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-    { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.browser } },
-    tseslint.configs.recommended,
-  ].concat([
+export default tseslint.config(
+  { ignores: ['coverage', 'dist'] },
+  js.configs.recommended,
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.browser } },
+  ...tseslint.configs.recommended,
+  ...[
     {
       rules: {
         '@typescript-eslint/no-unused-vars': [
@@ -25,5 +23,5 @@ export default defineConfig(
       },
     },
     { rules: { '@typescript-eslint/no-unused-expressions': 'off' } },
-  ]),
+  ],
 );
